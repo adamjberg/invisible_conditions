2ca83b664da7b8c58204e2d3a58452742a54ab4b
Adam Berg <adam@xyzdigital.com>
2017-03-13 22:42:00 -0700

    Submit form via ajax for more control

--- js/form.controller.js
+++ js/form.controller.js
@@ -3,14 +3,53 @@ $(document).ready(function () {
   var emailField = subscribeForm.find('input[type="email"]');
   var submitButton = subscribeForm.find('button');
   var emailError = subscribeForm.find('#email-error');
+  var interestField = $('select[name="INTEREST"]');
+  var commentsField = $('textarea[name="COMMENTS"]');
+  var submitResponseErrorText = $('#submit-error');
+  var submitResponseSuccessText = $('#submit-success');
 
   emailField.change(checkEmail);
 
   submitButton.click(function (event) {
     event.preventDefault();
 
-    checkEmail();
+    if (checkEmail()) {
+      var actionURL = subscribeForm.attr("action");
+      var newSubscriber = subscribeForm.serialize();
+
+      $.ajax({
+        type: "get",
+        url: "//invisibleconditions.us15.list-manage.com/subscribe/post-json?u=297ab086c5be06e3fcf7995e0&id=1e31923fde&c=?",
+        data: newSubscriber,
+        cache: false,
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        error: function (err) {
+          setSubmitText("Could not connect to the registration server. Please try again later.", true);
+        },
+        success: function (data) {
+          var success = data.result === "success";
+          if (success) {
+            setSubmitSuccessText(data.msg);
+          }
+          else {
+            setSubmitErrorText(data.msg);
+          }
+        }
+      });
+    }
   });
+
+  function setSubmitErrorText(text, error) {
+    submitResponseErrorText.html(text);
+    submitResponseErrorText.removeClass('hidden');
+  }
+
+  function setSubmitSuccessText(text, error) {
+    submitResponseSuccessText.html(text);
+    submitResponseSuccessText.removeClass('hidden');
+    subscribeForm.addClass('hidden');
+  }
 
   function checkEmail() {
     var email = emailField.val();
@@ -22,9 +61,6 @@ $(document).ready(function () {
     emailError.toggleClass("hidden", isValid);
     submitButton.prop('disabled', isInvalid);
 
-    if (isInvalid) {
-      // TODO: Submit form
-
-    }
+    return isValid;
   }
 });
