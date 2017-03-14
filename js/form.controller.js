56b622668b1b65e4f886e49884bbdd40a1de122c
Adam Berg <adam@xyzdigital.com>
2017-03-13 20:37:37 -0700

    Add email validation

--- js/form.controller.js
+++ js/form.controller.js
@@ -1,3 +1,30 @@
-$(document).ready(function() { 
-  console.log('hi');
+$(document).ready(function () {
+  var subscribeForm = $('#subscribeForm');
+  var emailField = subscribeForm.find('input[type="email"]');
+  var submitButton = subscribeForm.find('button');
+  var emailError = subscribeForm.find('#email-error');
+
+  emailField.change(checkEmail);
+
+  submitButton.click(function (event) {
+    event.preventDefault();
+
+    checkEmail();
+  });
+
+  function checkEmail() {
+    var email = emailField.val();
+
+    var isValid = validateEmail(email);
+    var isInvalid = !isValid;
+
+    emailField.toggleClass("error", isInvalid);
+    emailError.toggleClass("hidden", isValid);
+    submitButton.prop('disabled', isInvalid);
+
+    if (isInvalid) {
+      // TODO: Submit form
+
+    }
+  }
 });
