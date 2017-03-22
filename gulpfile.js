54f15cc8211248961dba1a6fd544a2cc4055b5d7
Adam Berg <adam@xyzdigital.com>
2017-03-21 20:33:06 -0700

    Add instagram feed

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,27 @@ var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var pug = require('gulp-pug');
 
-gulp.task('html', function() {
+gulp.task('html', function () {
   return gulp.src('templates/*.pug')
-  .pipe(pug({
-    // Your options in here. 
-  }))
-  .pipe(gulp.dest('public'));
+    .pipe(pug({
+      // Your options in here. 
+    }))
+    .pipe(gulp.dest('public'));
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp.src('scss/**/*.scss')
     .pipe(sourcemaps.init())  // Process the original sources
-      .pipe(sass())
+    .pipe(sass())
     .pipe(sourcemaps.write()) // Add the map to modified source.
     .pipe(gulp.dest('public/css'));
 });
 
 gulp.task('js', function () {
-  gulp.src('js/**/*.js')
+  gulp.src([
+    'node_modules/instafeed.js/instafeed.min.js',
+    'js/**/*.js'
+  ])
     .pipe(concat('app.js'))
     .pipe(gulp.dest('public/js'));
 });
