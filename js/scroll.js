f0aca9865bbacd92c1d48be357c28c6ce4885585
Adam Berg <adam@xyzdigital.com>
2017-03-26 15:34:33 -0700

    Add temp banner

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
+  var nav = $('nav');
+  var htmlBody = $('html,body');
+
+
   $('a').click(function (event) {
-
-
     var full_url = this.href;
     var parts = full_url.split("#");
 
@@ -12,9 +14,10 @@ $(document).ready(function () {
       var targetTop = 0;
       if (targetSelector) {
         targetTop = $("#" + targetSelector).offset().top;
+        targetTop -= nav.height();
       }
 
-      $('html,body').animate({ scrollTop: targetTop }, 'slow');
+      htmlBody.animate({ scrollTop: targetTop }, 'slow');
     }
 
   });
