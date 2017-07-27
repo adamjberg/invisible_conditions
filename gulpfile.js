f4df5cd185ce467bde22142a80ea25b5b29f1580
Adam Berg <adam@xyzdigital.com>
2017-07-26 20:59:20 -0700

    Start to support us page

--- gulpfile.js
+++ gulpfile.js
@@ -5,40 +5,44 @@ var concat = require('gulp-concat');
 var pug = require('gulp-pug');
 
 gulp.task('html', function () {
-    return gulp.src('templates/**/*.pug')
-        .pipe(pug({
-            // Your options in here. 
-        }))
-        .pipe(gulp.dest('public'));
+  return gulp.src([
+    'templates/index.pug',
+    'templates/be-a-mentor.pug',
+    'templates/support-us.pug'
+  ])
+    .pipe(pug({
+      // Your options in here. 
+    }))
+    .pipe(gulp.dest('public'));
 });
 
 gulp.task('sass', function () {
-    return gulp.src('scss/**/*.scss')
-        .pipe(sourcemaps.init())  // Process the original sources
-        .pipe(sass())
-        .pipe(sourcemaps.write()) // Add the map to modified source.
-        .pipe(gulp.dest('public/css'));
+  return gulp.src('scss/**/*.scss')
+    .pipe(sourcemaps.init())  // Process the original sources
+    .pipe(sass())
+    .pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe(gulp.dest('public/css'));
 });
 
 gulp.task('js', function () {
-    gulp.src([
-        'node_modules/instafeed.js/instafeed.min.js',
-        'js/**/*.js',
-        '!js/register.js'
-    ])
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest('public/js'));
+  gulp.src([
+    'node_modules/instafeed.js/instafeed.min.js',
+    'js/**/*.js',
+    '!js/register.js'
+  ])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('public/js'));
 
-    gulp.src(['js/register.js'])
-        .pipe(concat('register.js'))
-        .pipe(gulp.dest('public/js'));
+  gulp.src(['js/register.js'])
+    .pipe(concat('register.js'))
+    .pipe(gulp.dest('public/js'));
 });
 
 gulp.task('php', function () {
-    gulp.src([
-        'server/registration/*.php'
-    ])
-        .pipe(gulp.dest('public'));
+  gulp.src([
+    'server/registration/*.php'
+  ])
+    .pipe(gulp.dest('public'));
 })
 
 gulp.task('default', ['html', 'sass', 'js', 'php']);
