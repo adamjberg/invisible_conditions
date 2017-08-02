fbcb53803de3cac98cb5b2b45778f62ecd2c2d67
Adam Berg <adam@xyzdigital.com>
2017-08-02 08:47:35 -0700

    Add text/new banner image

--- js/carousel.js
+++ js/carousel.js
@@ -1,20 +1,30 @@
 $(document).ready(function () {
-   var carousel = $('#carousel');
+  var carousel = $('#carousel');
 
-   var images = ["img/banner1.jpeg", "img/banner2.jpeg", "img/banner3.jpeg"];
-   var imageIndex = 0;
+  var images = ["img/banner1.jpeg", "img/ComputerSignUp_Header.jpeg", "img/banner2.jpeg", "img/banner3.jpeg"];
+  var imageIndex = 0;
 
-   setTimeout(cycle, 6000);
+  setTimeout(cycle, 6000);
 
-   function cycle() {
-      imageIndex++;
-      if (imageIndex >= images.length) {
-         imageIndex = 0;
-      }
+  function cycle() {
+    imageIndex++;
+    if (imageIndex >= images.length) {
+      imageIndex = 0;
+    }
 
-      var imageUrl = images[imageIndex];
-      carousel.css('background-image', 'url(' + imageUrl + ')');
+    var imageUrl = images[imageIndex];
+    carousel.css('background-image', 'url(' + imageUrl + ')');
 
-      setTimeout(cycle, 5000);
-   }
+    var showText = imageIndex === 1;
+    if (showText) {
+      carousel.find('img').fadeOut();
+      carousel.find('h1').fadeIn();
+    }
+    else {
+      carousel.find('img').fadeIn();
+      carousel.find('h1').fadeOut();
+    }
+
+    setTimeout(cycle, 5000);
+  }
 });
