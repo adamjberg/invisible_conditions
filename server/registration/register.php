c625c6d4eee8a7855e2f3686ea3cb5b2073a4404
Adam Berg <adam@xyzdigital.com>
2017-08-20 00:00:38 -0700

    Add main mailing list checkbox

--- server/registration/register.php
+++ server/registration/register.php
@@ -24,39 +24,47 @@ else {
 
 $connection->close();
 
+function subscribe($list_id) {
+  include 'env.php';
+  $base_url = "https://us15.api.mailchimp.com/3.0";
+  $endpoint = "/lists/" . $list_id . "/members";
+  $url = $base_url . $endpoint;
+
+  $name = array(
+    "FNAME" => $_POST["first_name"],
+    "LNAME" => $_POST["last_name"]
+  );
+  $subscribe_data = array(
+    'email_address' => $_POST["email"],
+    "status" => "subscribed",
+    "merge_fields" => $name
+  );
+
+  $json_subscribe_data = json_encode($subscribe_data);
+
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $json_subscribe_data);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_USERPWD, "registration:" . $mailchimp_api_key);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json',
+      'Content-Length: ' . strlen($json_subscribe_data))
+  );
+
+  $result = curl_exec($ch);
+
+  curl_close($ch);
+}
+
 $mentor_list_id = "eb475a36e5";
 $participant_list_id = "4d3c48027c";
-$list_id = $_POST["type"] == "mentor" ? $mentor_list_id : $participant_list_id;
 
-$base_url = "https://us15.api.mailchimp.com/3.0";
-$endpoint = "/lists/" . $list_id . "/members";
-$url = $base_url . $endpoint;
+subscribe($_POST["type"] == "mentor" ? $mentor_list_id : $participant_list_id);
 
-$name = array(
-  "FNAME" => $_POST["first_name"],
-  "LNAME" => $_POST["last_name"]
-);
-$subscribe_data = array(
-	'email_address' => $_POST["email"],
-  "status" => "subscribed",
-  "merge_fields" => $name
-);
-
-$json_subscribe_data = json_encode($subscribe_data);
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $json_subscribe_data);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_USERPWD, "registration:" . $mailchimp_api_key);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'Content-Length: ' . strlen($json_subscribe_data))
-);
-
-$result = curl_exec($ch);
-
-curl_close($ch);
+if($_POST["mailing-list"] == "on") {
+  subscribe("1e31923fde");
+}
 
 ?>
