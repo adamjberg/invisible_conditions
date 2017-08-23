cb4de908ece76ebb0d222b7b6cd32426ef1c98d0
Adam Berg <adam@xyzdigital.com>
2017-08-22 17:09:29 -0700

    Update bios

--- js/members.js
+++ js/members.js
@@ -3,21 +3,21 @@ var members = [{
   last: "Lisaingo",
   title: "Chair / Funding & Outreach",
   quote: "He hopes this conference will allow him to give back some of the life lessons he has learnt along his journey.",
-  bio: "Simon has lived with Crohn’s disease for over 10 years. He hopes this conference will allow him to give back some of the life lessons he has learnt along his journey. His research examines how students overcome challenges they face at school and at home. Prior to commencing his graduate studies in school psychology, Simon worked as a middle school and high school teacher in the Yukon, Ontario, British Columbia."
+  bio: "Simon has lived with Crohn's disease for over 10 years. He hopes this conference will allow him to give back some of the life lessons he has learnt along his journey. His research examines how students overcome challenges they face at school and at home. Prior to commencing his graduate studies in school psychology, Simon worked as a middle school and high school teacher in the Yukon, Ontario, British Columbia."
 },
 {
   first: "Adam",
   last: "Huizinga",
   title: "Vice-Chair / Logistics & Mentorship",
   quote: "His hope is to share his experience not only managing the disease, but to de-stigmatize conversations.",
-  bio: "Adam was diagnosed with Crohn’s disease when he was 12 years old and developed anxiety and depression alongside his struggles with his illness. His hope is to share his experience not only managing the disease, but to de-stigmatize conversations about mental and physical illness, and how, in spite of these challenges, we can still live vibrant and meaningful lives."
+  bio: "Adam was diagnosed with Crohn's disease when he was 12 years old and developed anxiety and depression alongside his struggles with his illness. His hope is to share his experience not only managing the disease, but to de-stigmatize conversations about mental and physical illness, and how, in spite of these challenges, we can still live vibrant and meaningful lives."
 },
 {
   first: "Robyn",
   last: "McClure",
   title: "Content, Experts, & Evaluation",
   quote: "She believes this conference will offer an invaluable safe space for students to share their challenges with IBD.",
-  bio: "Robyn was diagnosed with Ulcerative Colitis three years ago, and since then has found a tremendous source of support amongst her peers with Crohn’s and Colitis. Based on that experience, she believes this conference will offer an invaluable safe space for students to share their challenges with IBD, while finding strength in the community and themselves. She hopes to contribute through her years of research and experiences working with youth and chronic illness. "
+  bio: "Robyn was diagnosed with Ulcerative Colitis three years ago, and since then has found a tremendous source of support amongst her peers with Crohn's and Colitis. Based on that experience, she believes this conference will offer an invaluable safe space for students to share their challenges with IBD, while finding strength in the community and themselves. She hopes to contribute through her years of research and experiences working with youth and chronic illness. "
 },
 {
   first: "Peter",
@@ -37,7 +37,8 @@ var members = [{
   first: "Karmen",
   last: "Lisaingo",
   title: "Logistics & Mentorship",
-  bio: ""
+  quote: "She hopes that this conference can help participants increase their self-advocacy skills while helping to build the community.",
+  bio: "Karmen is a UBC alumni who has been involved with the Crohn's and Colitis community since her partner was diagnosed with Crohn's disease over 10 years ago. She is interested in the role that friends and family can have in supporting youth with IBD as they become more independent. She hopes that this conference can help participants increase their self-advocacy skills while helping to build the community around them as they transition to adulthood."
 }];
 
 $(document).ready(function () {
