3c42d5dfe9df34cf5e33433ad3d7a5272f57b9c6
Adam Berg <adam@xyzdigital.com>
2017-08-24 17:03:30 -0700

    Update simon's qualifications

--- js/members.js
+++ js/members.js
@@ -4,7 +4,7 @@ var members = [{
   title: "Chair / Funding & Outreach",
   quote: "He hopes this conference will allow him to give back some of the life lessons he has learnt along his journey.",
   bio: "Simon has lived with Crohn's disease for over 10 years. He hopes this conference will allow him to give back some of the life lessons he has learnt along his journey. His research examines how students overcome challenges they face at school and at home. Prior to commencing his graduate studies in school psychology, Simon worked as a middle school and high school teacher in the Yukon, Ontario, British Columbia.",
-  qualifications: ""
+  qualifications: "PhD student, School Psychology Program, Faculty of Education, University of British Columbia"
 },
 {
   first: "Adam",
